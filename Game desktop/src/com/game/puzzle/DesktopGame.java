dcb6724f02e0fb2b789e50f903dd715d0c39f8d3
theSharque <thesharque@gmail.com>
2019-09-28 20:19:27 +0300

    Correct version

--- Game desktop/src/com/game/puzzle/DesktopGame.java
+++
@@ -1,9 +0,0 @@
-package com.game.puzzle;
-
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-
-public class DesktopGame {
-	public static void main(String[] args) {
-		new LwjglApplication(new MainGame( null ), "Game", 320, 480, false);
-	}
-}
--- Game main/src/com/game/puzzle/IActivityRequestHandler.java
+++
@@ -1,7 +0,0 @@
-package com.game.puzzle;
-
-public interface IActivityRequestHandler {
-
-	void showAds(boolean show);
-
-}
